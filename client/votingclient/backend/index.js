03e7d13fb6370e941653d777186a8f2c0e1e8c2a
munis-dot <muniyappan.k2002@gmail.com>
2024-01-10 20:44:31 +0530

    last commit

---
+++ client/votingclient/backend/index.js
@@ -0,0 +1,302 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const cors = require("cors");
+const port = 8080;
+const fs = require("fs");
+const imageModel = require("./models/models.js");
+const votermodel=require("./models/votermodel.js")
+const registerindex=require("./models/registerindex")
+const candidatemodel=require("./models/candidateregister.js");
+const { db } = require("./models/registerindex");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    'mongodb://127.0.0.1:27017/voting',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("connected successfully"))
+  .catch((err) => console.log("it has an error", err));
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  }),
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.match(/jpeg|jpg|png|gif$i/)) {
+      cb(new Error('File is not supported'), false);
+      return;
+    }
+    cb(null, true);
+  },
+}).fields([
+  { name: 'partyimg', maxCount: 1 },
+  { name: 'profileimg', maxCount: 1 },
+]);
+
+// Instances
+
+const server = http.createServer(app);
+
+// End point
+app.get('/',(req,res) => {
+  res.json("Api is working");
+})
+
+app.post('/addvoter',(req,res)=>{
+  const voter=votermodel({
+    address:req.body.ad,
+    alreadyvoted:false
+  });
+  voter.save()
+  .then(result=>{
+    res.send(result)
+  })
+  .catch(err=>console.log(err))
+})
+
+app.post('/updatevoter',(req,res)=>{
+  var myquery = { address: req.body.address };
+  var newvalues = { $push: {auctionlist: req.body.auction_id } };
+  usermodel.updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+})
+app.post('/addcandidate', upload, (req, res) => {
+  console.log(req.body)
+  const voter=candidatemodel({
+    partyname:req.body.name,
+    partysymbol:req.body.symbol,
+    candidatename:req.body.candidatename,
+    address:req.body.address,
+    partyimg:{
+      data: fs.readFileSync("uploads/" + req.files['partyimg'][0].filename),
+      contentType: "image/png",
+    },
+    profileimg:{
+      data: fs.readFileSync("uploads/" + req.files['profileimg'][0].filename),
+      contentType: "image/png",
+    },
+  });
+  voter.save()
+  .then(result=>{
+    res.send(result)
+  })
+  .catch(err=>console.log(err))
+})
+
+app.post('/checkvoter', async (req, res) => {
+  try {
+    const result = await votermodel.find({address:req.body.ad}, {alreadyvoted: 1});
+    console.log(result);
+    res.send(result);
+  } catch (err) {
+    throw err;
+  }
+});
+
+app.get('/getcandidate',async (req,res)=>{
+  try{
+    const result=await candidatemodel.find({});
+    // console.log(result)
+    res.send(result);
+  }catch(err){
+    throw err;
+  }
+})
+
+
+
+
+
+
+
+
+var keywords=[];
+app.post("/upload", (req, res) => {
+  console.log(req.body.tags)
+keywords=(req.body.tags).split(",");
+keywords.push("tfddrgr");
+  const saveImage =  imageModel({
+    Businessname:req.body.Businessname,
+    Owner:req.body.Owner,
+    address:req.body.address,
+    number:req.body.number,
+    landline:req.body.landline,
+    email:req.body.email,
+    url:req.body.url,
+    hours:req.body.hours,
+    hours2:req.body.hours2,
+    workdays:req.body.workdays,
+    about:req.body.about,
+    category:req.body.category,
+    sub_category:req.body.sub_category,
+    keyword: keywords,
+    img: {
+      data: fs.readFileSync("uploads/" + req.file.filename),
+      contentType: "image/png",
+    },
+  });
+  saveImage
+    .save()
+    .then((res) => {
+      console.log("image is saved");
+      console.log(typeof(keywords));
+    })
+    .catch((err) => {
+      console.log(err, "error has occur");
+    });
+    res.send('image is saved')
+});
+
+
+app.post('/search',async (req,res)=>{
+  var allData;
+  console.log(req.body);
+  if(req.body.searched !=undefined){
+    console.log(req.body.searched);
+  allData = await imageModel.find({keyword: {$in: [req.body.searchTerm]}});
+  }
+  else{
+    console.log("dSBFB ");
+    console.log(req.body.searched);
+    allData = await imageModel.find(); 
+  }
+  res.json(allData)
+  console.log("allData");
+})
+
+app.post('/addkeyword',(req,res)=>{
+  saveNewAddress(req.body)
+  .then(result => {
+    console.log(result);
+    res.send(result); 
+})
+})
+function saveNewAddress(address) {    
+  return new Promise((resolve, reject) => {
+    json.push(address)    
+    fs.writeFile('F:/project2/testing/public/address-list.json', JSON.stringify(json), (err) => {
+      if (err) reject(err)
+      resolve("File saved.")
+    })
+  });
+}
+
+app.post('/register',(req,res)=>{
+  const user =  usermodel({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password,
+  });
+  user
+    .save()
+    .then((result) => {
+      res.send(result);
+        })
+    .catch((err) => {
+      console.log(err, "error has occur");
+    });
+    })
+
+app.post('/login',(req,res)=>{
+   usermodel.findOne({email:req.body.email,password:req.body.password},(err,result)=>{
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+   })
+})
+
+app.post('/registerindex',(req,res)=>{
+  var myquery = { name: req.body.auctioner };
+  var newvalues = { $push: {auctionlist: req.body.auction_id } };
+  usermodel.updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+   
+  });
+  console.log(req.body.datetime)
+  var par=(req.body.particular).split(',');
+  const register =  registerindex({
+    auction_id:req.body.auction_id,
+    company:req.body.company,
+    time:req.body.datetime,
+    privacy:req.body.private,
+    passcode:req.body.passcode,
+    particular:par,
+    auctioner:req.body.auctioner
+  });
+  register
+    .save()
+    .then((result) => {
+      
+      res.send(result);
+        })
+    .catch((err) => {
+      console.log(err, "error has occur");
+    });
+
+})
+
+app.post('/auctiondetail',upload,(req,res)=>{
+  console.log(req.body.auction_id)
+  const aregister =  auctionregister({
+    auction_id:req.body.auction_id,
+    particular_name:req.body.particular_name,
+   // bitter:req.body.bitter,
+   startingamt:req.body.startingamt,
+   // bidamt:req.body.bidamt,
+   // conversation:req.body.conversation,
+    img: {
+      data: fs.readFileSync("uploads/" + req.file.filename),
+      contentType: "image/png",
+    },
+  });
+  aregister
+    .save()
+    .then((result) => {
+      res.send(result)
+        })
+    .catch((err) => {
+      console.log(err, "error has occur");
+    });
+})
+
+app.get('/getauctions',(req,res)=>{
+  registerindex.find({},(err,result)=>{
+   if (err) throw err;
+   
+   res.send(result)
+  })
+})
+
+app.post('/auctionnow',(req,res)=>{
+  
+  console.log(req.body.auctionid)
+  auctionregister.find({auction_id:req.body.auctionid},(err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+app.post('/finishing',(req,res)=>{
+  console.log(req.body)
+auctionregister.updateOne({auction_id:req.body.auction_id,particular_name:req.body.itemname},{ $set: {bitter: req.body.bitter,bidamt:req.body.bidamt } },(err,result)=>{
+  if(err) throw err;
+  res.send(result);
+})
+})
+
+server.listen(8000,() => console.log('Server started on 8000'))
---
+++ client/votingclient/src/components/tables.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react'
+import { useContract, useContractRead,useContractWrite } from "@thirdweb-dev/react";
+import axios from 'axios';
+function tables() {
+   const[candidate,setcandidate]=useState([{profileimg:{data:{data:78797}},partyimg:{data:{data:78797}}},{profileimg:{data:{data:78797}},partyimg:{data:{data:78797}}}]);
+  
+   useEffect(()=>{
+      axios.get("http://localhost:8000/getcandidate")
+      .then(res=>{
+         console.log(res)
+         setcandidate(res.data)})
+      .catch()
+   },[])
+
+   const { contract } = useContract("0xa513E6E4b8f2a923D98304ec87F64353C4D5C853");
+   const { data, isLoading } = useContractRead(contract, "getcanditates")
+   const { mutateAsync: vote, isLoadings } = useContractWrite(contract, "vote")
+   let candidates=data;
+   console.log(candidates)
+
+   const call = async (index) => {
+      try {
+        const data = await vote({ args: [index] });
+        console.info("contract call successs", data);
+        alert("voted successfully")
+      } catch (err) {
+        console.error("contract call failure", err);
+        alert(err);
+      }
+    }
+
+   function arrayBufferToBase64(buffer) {
+      var binary = '';
+      var bytes = [].slice.call(new Uint8Array(buffer));
+      bytes.forEach((b) => binary += String.fromCharCode(b));
+      return window.btoa(binary);
+    };
+
+    const votes=(who)=>{
+      console.log(who)
+      const index = candidates.findIndex(item => item.name === who);
+      console.log(index)
+      call(index);
+    }
+
+
+
+  return (
+    <div>
+         <div class="relative overflow-x-auto rounded-lg shadow-md ">
+        <table class="w-4/5 text-sm text-left rounded-lg border-collapse border border-slate-400 0 border-collapse border border-slate-400  m-10 ml-36" style={{border:"1px solid black"}}>
+        <thead class="text-xs text-white uppercase h-16 border-black bg-slate-60 dark:text-gray-400" >
+        <tr style={{border:"1px solid black"}} className='bg-slate-600'>
+        <th scope="col" class="px-6 py-3">
+                    Party logo
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    profile
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Symbol
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Party Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    candidate name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Votes
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+         {candidate.map((_id)=>{
+            var base64String=arrayBufferToBase64(_id.partyimg.data.data)
+            var base64String2=arrayBufferToBase64(_id.profileimg.data.data)
+            return(
+                <tr class="border border-black">               
+                <td class="px-6 py-4">                  
+                       <img alt='image' src={"data:image/png;base64," + base64String}  className='bg-grey-800'  style={{borderRadius:'50%',width:"95px"}}></img>  
+                       </td>
+                    <td class="px-6 py-4">
+                       
+                       <img alt='image' src={"data:image/png;base64," + base64String2}  className='bg-grey-800'  style={{borderRadius:'50%',width:"95px"}}></img>
+       
+                       </td>
+                       <th scope="row" class="px-6 py-4 font-medium text-xl text-white whitespace-nowrap dark:text-white">
+                       {_id.partysymbol}
+                    </th>
+                    <th scope="row" class="px-6 py-4 font-medium text-xl text-white whitespace-nowrap dark:text-white">
+                       {_id.partyname}
+                    </th>
+                    <th scope="row" class="px-6 py-4 font-medium text-xl text-white whitespace-nowrap dark:text-white">
+                       {_id.candidatename}
+                    </th>
+                       <th scope="row" class="px-6 py-4 font-medium text-xl text-white whitespace-nowrap dark:text-white">
+                       <button onClick={e=>votes(_id.partyname)} class="text-base bg-white p-3 rounded px-8 text-blue-600 dark:text-blue-500 hover:bg-blue-500 hover:text-white">Vote</button>
+                    </th>
+                </tr>
+            )
+         })}
+           
+         
+            
+        </tbody>
+    </table>
+</div>
+
+    </div>
+  )
+}
+
+export default tables
